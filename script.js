661a723083fd035a4e68bcc2acd4c2eb436f7ce0
purnima11apr <141928600+purnima11apr@users.noreply.github.com>
2023-09-28 15:19:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let paragraph = document.getElementById("status");
+const button = document.getElementById("enterbtn");
+ let result = document.getElementById("result");
+
+enterBtn.addEventListener("click", function () {
+            let originalText = paragraph.textContent;
+            paragraph.style.display = "none"; 
+            result.textContent = originalText;
+        });
+
+	
