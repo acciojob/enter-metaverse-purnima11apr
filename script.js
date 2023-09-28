96b25693e353649928c2f0358e00aaae4fc7c144
purnima11apr <141928600+purnima11apr@users.noreply.github.com>
2023-09-28 15:30:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //your JS code here. If required.
-let paragraph = document.getElementById("status");
-const button = document.getElementById("enterbtn");
- let result = document.getElementById("result");
+   let paragraph = document.getElementById("status");
+        const enterBtn = document.getElementById("enterBtn");
+        let result = document.getElementById("result");
 
-enterBtn.addEventListener("click", function () {
-            let originalText = paragraph.textContent;
-            paragraph.style.display = "none"; 
-            result.textContent = originalText;
+        enterBtn.addEventListener("click", function () {
+            // Change the text of the paragraph
+            paragraph.textContent = "Entered Metaverse";
+
+            // Display the updated text in the h1 tag
+            result.textContent = paragraph.textContent;
         });
-
-	
