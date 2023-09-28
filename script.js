133a8cc6c96496c561c33f80e52d375281e69733
purnima11apr <141928600+purnima11apr@users.noreply.github.com>
2023-09-28 15:49:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 //your JS code here. If required.
    let paragraph = document.getElementById("status");
         const enterBtn = document.getElementById("enterBtn");
-        let result = document.getElementById("result");
+       
 
         enterBtn.addEventListener("click", function () {
-            // Set the text of the h1 element to "Entered Metaverse"
-			paragraph.style.display ="none";
-            result.textContent = "Entered Metaverse";
+			 let result = document.createElement("h1");
+			  result.textContent = "Entered Metaverse"
+		     	paragraph.appendChild(result);
+			
         });
